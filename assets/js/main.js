22e3a4c9d904231211a4ecc01cc203705c8d0eae
NicholaiGontcharov <42595279+NicholaiGontcharov@users.noreply.github.com>
2025-07-17 07:39:04 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -338,4 +338,20 @@
 
 						});
 
+	let lastScrollTop = 0;
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', function() {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    // Scrolling down
+    navbar.style.top = '-70px'; // Adjust value to your navbar's height
+  } else {
+    // Scrolling up
+    navbar.style.top = '0';
+  }
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+}, false);
+
 })(jQuery);
